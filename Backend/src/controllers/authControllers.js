2f1ca9ae42eba4c5e71894f3a0244ac764bfffb4
gpalakgupta <palakgupta5709@gmail.com>
2025-08-20 23:57:37 +0530

    initial commit

--- Backend/src/controllers/authControllers.js
+++ Backend/src/controllers/authControllers.js
@@ -30,3 +30,34 @@ export const register = async (req, res) => {
         res.status(500).json({ message: "Server error" });
     }
 };
+
+
+// login
+
+export const login = async (req, res) => {
+    const {email, password} = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        const isMatch = await user.comparePassword(password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            token
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+    }
+}
