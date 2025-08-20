f787242c5e3007475608fae007d8ba2017fbf5a1
gpalakgupta <palakgupta5709@gmail.com>
2025-08-21 00:06:44 +0530

    initial commit

---
+++ Backend/src/models/bookModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    stock: {
+        type: Number,
+        default: 1
+    },
+    image: {
+        type: String
+    }
+}, { timestamps: true });
+
+export const Book = mongoose.model("Book", bookSchema);
+export default Book;
